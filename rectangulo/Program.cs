9845cee2ea4fc8d3476214954b9fc6f20dfe0407
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-11-19 19:33:01 -0300

    clase 19-11- tarea rectangulo
    clase 19-11- tarae rectangulo

---
+++ rectangulo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            // imprimir la parte superior del rectangulo
+            while(contador < 10)
+            {
+                Console.Write("A");
+                contador++;
+            }
+
+            // repetir 5 veces          
+            int linea = 0;
+            while (linea < 5) {
+
+                // imprimir la linea "B         B"
+                Console.WriteLine();
+                Console.Write("B");
+                contador = 0;
+                while(contador < 8)
+                {
+                    Console.Write(" ");
+                    contador++;
+                }
+                Console.Write("B");
+
+                linea++;
+            }
+
+            // imprime la parte inferior del rectangulo
+            Console.WriteLine();
+            contador = 0;
+            while (contador < 10)
+            {
+                Console.Write("A");
+                contador++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
