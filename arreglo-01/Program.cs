b29368750dfdf1787e54c229ab2938cb04ffc42c
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-11-19 22:19:34 -0300

    clase 19-11-2020
    mi-primer-arreglo

---
+++ arreglo-01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = new int[6];
+
+            notas[0] = 2; // asignar un valor a la primera casilla
+            notas[1] = 4;// asignar un valor a la segunda casilla
+            notas[2] = 5;
+            notas[3] = 3;
+            notas[4] = 7;
+            notas[5] = 6;
+
+            int contador = 0;
+            while(contador < 6)
+            {
+                Console.WriteLine(notas[contador]);
+                contador++;
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
