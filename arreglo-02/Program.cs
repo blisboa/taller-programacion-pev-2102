4dbdf1e8ca0e40b6d4e8f374735c296e1f86010a
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-11-21 11:16:34 -0300

    clase 21-11-2020
    clase 21-11-2020

---
+++ arreglo-02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = new int[6];
+
+            notas[0] = 4; // en C# la primera casilla se indica con la posicion 0
+            notas[1] = 5;
+            notas[2] = 6;
+            notas[3] = 3;
+            notas[4] = 2;
+            notas[5] = 7; // la ultima casilla se indica con la posicion N-1, N la dimension ( la cantidad total de casillas) del arreglo 
+
+            int contador = 0;
+            // mostrar el contenido del arreglo
+            Console.WriteLine("El contenido del arreglo es:");
+            while (contador < 6)
+            {
+                Console.WriteLine(notas[contador]);
+                contador++;
+            }
+
+            // encontra un elemento y mostrar la posicion donde se encontro ese elemento
+            int valorBuscado = 4;
+            contador = 0; // volver a inicializar la variable contador en cero
+            while(contador < 6) // Si contador < 6 es Verdad el ciclo while continua
+                                // Si contador < 6 es Falso el ciclo while NO continua, se termina el ciclo
+            {
+                Console.WriteLine("Comparar el elemento notas[" + contador + "]");
+                if (notas[contador] == valorBuscado)
+                {
+                    Console.WriteLine("El elemento buscado (" + valorBuscado + ") está en la posicion " + contador);
+                    contador = 1000; // Asignar un valor que haga que la condicion "contador < 6" sea Falsa
+                }
+                contador++;
+            }
+
+            Console.WriteLine("Fin de programa");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ buscar.menor-elemento/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buscar.menor_elemento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[6];
+            int contador;
+            Random aleatorio = new Random() ;
+
+            do
+            {
+                Console.Clear();
+                // asignar valore aleatorios a cada elemento del arreglo
+                for (contador = 0; contador < 6; contador++)
+                {
+                    arreglo[contador] = aleatorio.Next(1, 100);
+                }
+
+                // mostrar el arreglo
+                Console.WriteLine("Contenido del arreglo:");
+                for (contador = 0; contador < 6; contador++)
+                {
+                    Console.Write(arreglo[contador] + " ");
+                }
+                Console.WriteLine();
+
+                // buscar el menor elemento
+                int menor;
+                // Comparra el primer elemenot con el segundo elemento
+                if (arreglo[0] < arreglo[1])
+                    menor = arreglo[0];
+                else
+                    menor = arreglo[1];
+
+                for (contador = 2; contador < 6; contador++)
+                {
+                    if (arreglo[contador] < menor) // si encontramos un nuevo MENOR
+                    {
+                        // antiguo menor = nuevo menor
+                        menor = arreglo[contador];
+                    }
+                }
+
+                Console.WriteLine("El menor elemento del arreglo es:" + menor);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Si desea ejecutar nuevamente, presiones la tecla S, para terminar presiones cualquier otra tecla.");
+            } while (Console.ReadKey().Key == ConsoleKey.S);
+        }
+    }
+}
