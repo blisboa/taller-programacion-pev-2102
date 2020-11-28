b4f5c36607308dcc61262aafb0a8f7d239eb5fd4
bernardo.lisboa <bernardo.lisboa@sonda.com>
2020-11-28 18:56:54 -0300

    clase 28-11-2020
    Traductor Ingles/Español

---
+++ traductorWin/frmTraductor.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace traductorWin
+{
+    partial class frmTraductor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTraducir = new System.Windows.Forms.Button();
+            this.txtFraseEspanol = new System.Windows.Forms.TextBox();
+            this.lblFraseEspañol = new System.Windows.Forms.Label();
+            this.txtFraseIngles = new System.Windows.Forms.TextBox();
+            this.lblFraseIngles = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTraducir
+            // 
+            this.btnTraducir.Location = new System.Drawing.Point(59, 101);
+            this.btnTraducir.Name = "btnTraducir";
+            this.btnTraducir.Size = new System.Drawing.Size(75, 23);
+            this.btnTraducir.TabIndex = 0;
+            this.btnTraducir.Text = "Traducir";
+            this.btnTraducir.UseVisualStyleBackColor = true;
+            this.btnTraducir.Click += new System.EventHandler(this.btnTraducir_Click);
+            // 
+            // txtFraseEspanol
+            // 
+            this.txtFraseEspanol.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFraseEspanol.Location = new System.Drawing.Point(59, 53);
+            this.txtFraseEspanol.Name = "txtFraseEspanol";
+            this.txtFraseEspanol.Size = new System.Drawing.Size(464, 35);
+            this.txtFraseEspanol.TabIndex = 1;
+            // 
+            // lblFraseEspañol
+            // 
+            this.lblFraseEspañol.AutoSize = true;
+            this.lblFraseEspañol.Location = new System.Drawing.Point(56, 37);
+            this.lblFraseEspañol.Name = "lblFraseEspañol";
+            this.lblFraseEspañol.Size = new System.Drawing.Size(88, 13);
+            this.lblFraseEspañol.TabIndex = 2;
+            this.lblFraseEspañol.Text = "Frase en español";
+            // 
+            // txtFraseIngles
+            // 
+            this.txtFraseIngles.Font = new System.Drawing.Font("Segoe UI", 15.75F);
+            this.txtFraseIngles.Location = new System.Drawing.Point(59, 154);
+            this.txtFraseIngles.Name = "txtFraseIngles";
+            this.txtFraseIngles.Size = new System.Drawing.Size(464, 35);
+            this.txtFraseIngles.TabIndex = 3;
+            // 
+            // lblFraseIngles
+            // 
+            this.lblFraseIngles.AutoSize = true;
+            this.lblFraseIngles.Location = new System.Drawing.Point(59, 135);
+            this.lblFraseIngles.Name = "lblFraseIngles";
+            this.lblFraseIngles.Size = new System.Drawing.Size(79, 13);
+            this.lblFraseIngles.TabIndex = 4;
+            this.lblFraseIngles.Text = "Frase en Ingles";
+            // 
+            // frmTraductor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 235);
+            this.Controls.Add(this.lblFraseIngles);
+            this.Controls.Add(this.txtFraseIngles);
+            this.Controls.Add(this.lblFraseEspañol);
+            this.Controls.Add(this.txtFraseEspanol);
+            this.Controls.Add(this.btnTraducir);
+            this.Name = "frmTraductor";
+            this.Text = "Traductor";
+            this.Load += new System.EventHandler(this.frmTraductor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTraducir;
+        private System.Windows.Forms.TextBox txtFraseEspanol;
+        private System.Windows.Forms.Label lblFraseEspañol;
+        private System.Windows.Forms.TextBox txtFraseIngles;
+        private System.Windows.Forms.Label lblFraseIngles;
+    }
+}
+
---
+++ traductorWin/frmTraductor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace traductorWin
+{
+    public partial class frmTraductor : Form
+    {
+        string[] dic_espanol = new string[20];
+        string[] dic_ingles = new string[20];
+
+        // constructor, es lo primero que se ejcuta cuando se crea el objeto
+        public frmTraductor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTraductor_Load(object sender, EventArgs e)
+        {
+            dic_espanol[0] = "hola";
+            dic_espanol[1] = "mundo";
+            dic_espanol[2] = "dia";
+            dic_espanol[3] = "lunes";
+            dic_espanol[4] = "martes";
+            dic_espanol[5] = "miercoles";
+            dic_espanol[6] = "jueves";
+            dic_espanol[7] = "viernes";
+            dic_espanol[8] = "sabado";
+            dic_espanol[9] = "domingo";
+            dic_espanol[10] = "hoy";
+            dic_espanol[11] = "es";
+
+            dic_ingles[0] = "hello";
+            dic_ingles[1] = "world";
+            dic_ingles[2] = "day";
+            dic_ingles[3] = "monday";
+            dic_ingles[4] = "tuesday";
+            dic_ingles[5] = "wednesday";
+            dic_ingles[6] = "thursday";
+            dic_ingles[7] = "friday";
+            dic_ingles[8] = "saturday";
+            dic_ingles[9] = "sunday";
+            dic_ingles[10] = "today";
+            dic_ingles[11] = "is";
+
+
+        }
+
+        private void btnTraducir_Click(object sender, EventArgs e)
+        {
+            string palabraBuscada;
+            int pos_palabra_buscada;
+            string[] palabras = new string[20];
+
+            palabras = txtFraseEspanol.Text.Split(' ');
+            txtFraseIngles.Text = "";
+            for (int contador = 0; contador < palabras.Length; contador++)
+            {
+
+                palabraBuscada = palabras[contador];
+                // buscar la posicion de la palaba buscada
+                pos_palabra_buscada = -1;
+                for (int pos = 0; pos < dic_espanol.Length; pos++)
+                {
+                    if (palabraBuscada == dic_espanol[pos])
+                    {
+                        pos_palabra_buscada = pos;
+                    }
+                }
+                //mostrar la palabra traducidad
+                if (pos_palabra_buscada >= 0) // Significa que la palabra se encontro
+                    txtFraseIngles.Text = txtFraseIngles.Text + " " + dic_ingles[pos_palabra_buscada];
+                else
+                    txtFraseIngles.Text = txtFraseIngles.Text + " " + palabraBuscada;
+
+            }
+        }
+    }
+}
